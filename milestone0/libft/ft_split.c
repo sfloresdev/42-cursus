6923cf034d577748d786b55dc0bbb02c9f7f3975
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2025-10-22 13:08:03 +0200

    FIN, split arreglado en principio pasa todos los tests

--- milestone0/libft/ft_split.c
+++ milestone0/libft/ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seflores <seflores@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: seflores <seflores@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 21:14:23 by seflores          #+#    #+#             */
-/*   Updated: 2025/10/21 19:14:38 by seflores         ###   ########.fr       */
+/*   Updated: 2025/10/22 13:07:03 by seflores         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,6 @@ static size_t	count_words(char const *s, char c)
 	return (words);
 }
 
-static void	get_first_word(char const *s, char c, int *index)
-{
-	*index = 0;
-	while (s[*index] && s[*index] == c)
-		(*index)++;
-	*index -= 1;
-}
-
 static char	**free_pointers(char **array)
 {
 	size_t	i;
@@ -59,74 +51,41 @@ static char	**free_pointers(char **array)
 	return (NULL);
 }
 
+static int	check_null(char ***array, char const *s, char c)
+{
+	if (!s)
+		return (0);
+	*array = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!*array)
+		return (0);
+	return (1);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	**array;
-	int		i;
-	int		word;
-	int		start;
+	char		**array;
+	size_t		i;
+	size_t		word;
+	size_t		start;
 
-	if (!s)
-		return (NULL);
-	array = malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!array)
+	if (!check_null(&array, s, c))
 		return (NULL);
 	word = 0;
-	get_first_word(s, c, &i);
-	while (s[++i])
+	i = 0;
+	while (s[i])
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+		while (s[i] && s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
 		{
-			array[word] = ft_substr(&s[i], 0, (i - start) + 1);
+			array[word] = ft_substr(s, start, (i - start));
 			if (!array[word++])
 				return (free_pointers(array));
-			while (s[i + 1] && s[i + 1] == c)
-				start = i++ + 1;
 		}
 	}
 	array[word] = NULL;
 	return (array);
 }
-
-/* #include <stdio.h>
-
-int	main(void)
-{
-// subtest 6: NULL string
-	{
-		const char *s = NULL;
-		char c = ' ';
-		char **got = ft_split(s, c);
-
-		if (got == NULL) printf("OK");
-		else
-		{
-			printf("expected NULL for NULL string");
-			int k = 0;
-			while(got[k])
-			{
-				free(got[k]);
-				k++;
-			}
-			free(got);
-		}
-	}
-	return (0);
-} */
-
-/*
-int	main(void)
-{
-	char	*string = "hola que tal estas";
-	char	**res;
-
-	res = ft_split(string, ' ');
-
-	while (*res)
-	{
-		printf("%s\n", *res);
-		*res++;
-	}
-
-	return (0);
-}*/
