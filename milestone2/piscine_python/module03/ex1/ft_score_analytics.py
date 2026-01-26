0b8c6082463256650683dbf8d0505431e97ce997
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2026-01-26 21:40:56 +0100

    Hoy no era un buen dia para programar

--- milestone2/piscine_python/module03/ex1/ft_score_analytics.py
+++ milestone2/piscine_python/module03/ex1/ft_score_analytics.py
@@ -12,14 +12,33 @@ import sys
 def score_analytics():
     print("=== Player Score Analytics ===")
     total_players = len(sys.argv) - 1
-    if total_players > 1:
-        total_score = 0
-        range = 0
+    if total_players >= 1:
+        scores = []
         for score in sys.argv[1:]:
-            total_score = sum(score)
-        average = total_players / total_players
-        high_score = max()
-        min_score  = min() 
-        
+            try:
+                points = int(score)
+                scores.append(points)
+            except ValueError:
+                print("Caught ValueError: invalidad literal for int()\n")
+        if not scores:
+            print("No scores were processed")
+            return
+        print(f"Scores processed: {scores}")
+        total_scores = sum(scores)
+        average = total_scores / len(scores)
+        high_score = max(scores)
+        low_score = min(scores)
+        score_range = high_score - low_score
+        print(f"Total players: {len(scores)}")
+        print(f"Total score: {total_scores}")
+        print(f"Average score: {average}")
+        print(f"High score: {high_score}")
+        print(f"Low score: {low_score}")
+        print(f"Score range: {score_range}")
+    else:
+        print("No scores provided. Usage: python3", end="")
+        print(" ft_score_analytics.py <score1> <score2> ...")
 
-            
+
+if __name__ == "__main__":
+    score_analytics()
