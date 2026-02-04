ad3abe432362d736887f1e9fb9b1d89093697b03
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2026-02-04 17:13:54 +0100

    ahora si module00

--- milestone2/piscine_python/module00/ex6/ft_garden_summary.py
+++ milestone2/piscine_python/module00/ex6/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary():
-    garden_name = str(input("Enter garden name: "))
-    n_plants = int(input("Enter number of plants: "))
+    garden_name = input("Enter garden name: ")
+    n_plants = input("Enter number of plants: ")
     print(f"Garden: {garden_name}")
     print(f"Plants: {n_plants}")
     print("Status: Growing well!")
