68e927a3c6824f35904412d0b24e5b9fc0509db7
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2026-04-16 17:14:36 +0200

    Add: Module08 ready for evaluation

--- milestone2/piscine_python/module08/ex2/oracle.py
+++ milestone2/piscine_python/module08/ex2/oracle.py
@@ -1,51 +1,55 @@
 import os
-import sys
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # type: ignore
+
 
 def consult_oracle() -> None:
-	print("ORACLE STATUS: Reading the Matrix...")
+    print("ORACLE STATUS: Reading the Matrix...")
 
-	mode = os.getenv("MATRIX_MODE", "development")
-	db_url = os.getenv("DATABASE_URL")
-	api_key = os.getenv("API_KEY")
-	logs = os.getenv("LOG_LEVEL")
-	endpoint = os.getenv("ZION_ENDPOINT")
+    env_loaded: bool = load_dotenv()
 
-	if not all([db_url, api_key, logs, endpoint]):
-		print("WARNING: Missing configuration! Some system might not work")
+    mode = os.getenv("MATRIX_MODE", "development")
+    db_val = os.getenv("DATABASE_URL")
+    api_val = os.getenv("API_KEY")
+    logs = os.getenv("LOG_LEVEL")
+    endpoint = os.getenv("ZION_ENDPOINT")
 
-	print("\nConfiguration Loaded:")
-	print(f"Mode: {mode}")
-	if mode == "production":
-		db_status = "Connected to Remote Server"
-		api_key = "Auth via SSL"
-	else:
-		db_status = "Connected to local instance"
-		api_key = "Auth"
+    if not all([db_val, api_val, logs, endpoint]):
+        print("WARNING: Missing configuration! Some system might not work")
 
-	print(f"Database: {db_status}")
-	print(f"API Access: {api_key if api_key else 'Access Denied'}")
-	print(f"Log level: {logs if logs else 'NOT SET'}")
-	print(f"Zion network: { 'Online' if endpoint else 'Offline'}")
+    print("\nConfiguration Loaded:")
+    print(f"Mode: {mode}")
 
+    db_status: str
+    api_key: str
+    if mode == "production":
+        db_status = "Connected to Remote Server" if db_val else "Disconnected"
+        api_key = "Auth via SSL" if api_val else 'Access Denied'
+    else:
+        db_status = "Connected to local instance" if db_val else "Disconnected"
+        api_key = "Auth" if api_val else 'Access Denied'
 
-	print("\nEnvironment security check:")
-	if api_key and db_url:
-		print("[OK] No hardcoded secrets detected")
-	else:
-		print("[!] Security risk: Critical secrets missing")
-	
-	if env_loaded:
-		print("[OK] .env file loaded")
-	else:
-		print("[!] .env file not detected")
+    print(f"Database: {db_status}")
+    print(f"API Access: {api_key}")
+    print(f"Log level: {logs if logs else 'NOT SET'}")
+    print(f"Zion network: {'Online' if endpoint else 'Offline'}")
 
-	if mode == "production":
-		print("[OK] Production overrides available")
-	else:
-		print("[!] Default settings active")
-	print("The Oracle sees all configurations.")
+    print("\nEnvironment security check:")
+    if api_val and db_val:
+        print("[OK] No hardcoded secrets detected")
+    else:
+        print("[!] Security risk: Critical secrets missing")
+
+    if env_loaded:
+        print("[OK] .env file loaded")
+    else:
+        print("[! ] .env file not detected")
+
+    if mode == "production":
+        print("[OK] Production overrides available")
+    else:
+        print("[!] Default settings active")
+    print("The Oracle sees all configurations.")
 
 
 if __name__ == "__main__":
-	consult_oracle()
+    consult_oracle()
